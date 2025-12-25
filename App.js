53a6a8ed02bcaf060a147a5dbc97ef847ed9e027
TAZIKenza <kenzatazi91@gmail.com>
2025-12-25 14:17:36 +0100

    TP6

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
+// import { createSlice } from "@reduxjs/toolkit"; 
+// const todosSlice = createSlice({ 
+// name: "todos", 
+// initialState: [], 
+// reducers: { 
+// addTodo: (state, action) => { 
+// state.push(action.payload); 
+// }, 
+// removeTodo: (state, action) => { 
+//      return state.filter((t) => t.id !== action.payload); 
+//    }, 
+//  }, 
+// }); 
+ 
+// export const { addTodo, removeTodo } = todosSlice.actions; 
+// export default todosSlice.reducer;
 import React, { useContext } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AuthProvider, { AuthContext } from "./context/AuthContext";
 import AppDrawer from "./navigation/AppDrawer";
 import LoginScreen from "./screens/LoginScreen";
 
-export default function App() {
-  return (
-    <AuthProvider>
-      <NavigationContainer>
-        <RootNavigator />
-      </NavigationContainer>
-    </AuthProvider>
-  );
-}
-
-function RootNavigator() {
-  const { user } = useContext(AuthContext);
-
-  return user ? <AppDrawer /> : <LoginScreen />;
-}
+ 
+import { Provider } from "react-redux"; 
+import { store } from "./store/store"; 
+ 
+function RootNavigator() { 
+ const { user } = useContext(AuthContext); 
+ 
+ return user ? <AppDrawer /> : <LoginScreen />; 
+} 
+ 
+export default function App() { 
+ return ( 
+   <Provider store={store}> 
+     <AuthProvider> 
+       <NavigationContainer> 
+         <RootNavigator /> 
+       </NavigationContainer> 
+     </AuthProvider> 
+   </Provider> 
+ ); 
+} 
--- screens/TodoListScreen.js
+++ screens/TodoListScreen.js
@@ -1,44 +1,30 @@
-import { useState, useEffect } from "react"; 
-import { View, Text, Button, FlatList, TouchableOpacity } from "react-native"; 
- import AppBar from "./AppBar";
+import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import { useEffect } from "react";
+import AppBar from "../components/AppBar";
+import { useTodoStore } from "../store/useTodoStore";
+//import { useSelector, useDispatch } from "react-redux"; 
+//import { addTodo } from "../store/todosSlice"; 
 
-export default function TodoListScreen({ navigation }) { 
- const [todos, setTodos] = useState([]); 
- const [loading, setLoading] = useState(true); 
- 
- useEffect(() => { 
-   console.log("Chargement des tâches..."); 
- 
-   setTimeout(() => { 
-     setTodos([ 
-       { id: 1, title: "Faire les courses" }, 
-       { id: 2, title: "Sortir le chien" }, 
-       { id: 3, title: "Coder une app RN" }, 
-     ]); 
-     setLoading(false); 
-   }, 1000); 
- }, []); // [] => exécute une seule fois au montage 
- 
- if (loading) { 
-   return ( 
-     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}> 
-       <Text style={{ fontSize: 20 }}>Chargement...</Text> 
-     </View> 
-   ); 
- } 
- 
- return (
-  <View style={{ flex: 1 }}>
-    {/* AppBar en haut */}
-    <AppBar title="Mes tâches" />
+export default function TodoListScreen({ navigation }) {
+  const { todos, addTodo } = useTodoStore();
 
-    {/* Contenu */}
+  useEffect(() => {
+    addTodo({ id: 1, title: "Faire les courses" });
+    addTodo({ id: 2, title: "Sortir le chien" });
+    addTodo({ id: 3, title: "Coder une app RN" });
+  }, []);
+
+  return (
     <View style={{ flex: 1, padding: 20 }}>
+      <AppBar title="Mes tâches" />
+
       <FlatList
         data={todos}
         keyExtractor={(i) => i.id.toString()}
         renderItem={({ item }) => (
-          <TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Détails", item)}
+          >
             <Text style={{ padding: 10, fontSize: 18 }}>
               {item.title}
             </Text>
@@ -46,7 +32,5 @@ export default function TodoListScreen({ navigation }) {
         )}
       />
     </View>
-  </View>
-);
-
-} 
+  );
+}
